ed2edca8c9a86c3f42b20ffabc5df9e01116aabe
Bobby Shu <shu.yun@northeastern.edu>
2023-05-11 14:01:13 +0800

    React state

---
+++ next_js_study/JStoReact/App.js
@@ -0,0 +1,47 @@
+/*
+ * @Author: Bobby Shu shu.yun@northeastern.edu
+ * @Date: 2023-05-11 13:54:37
+ * @LastEditors: Bobby Shu shu.yun@northeastern.edu
+ * @LastEditTime: 2023-05-11 13:55:26
+ * @FilePath: \Project-Web\next_js_study\JStoReact\App.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { useState } from 'react';
+import { sculptureList } from './data.js';
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleNextClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? 'Hide' : 'Show'} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+    </>
+  );
+}
