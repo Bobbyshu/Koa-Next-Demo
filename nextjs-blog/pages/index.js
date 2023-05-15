f8cd0979b167a571a0378330776852daa85e47e6
Bobby Shu <shu.yun@northeastern.edu>
2023-05-15 14:46:48 +0800

    update

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,7 +2,7 @@
  * @Author: Bobby Shu shu.yun@northeastern.edu
  * @Date: 2023-05-12 14:29:49
  * @LastEditors: Bobby Shu shu.yun@northeastern.edu
- * @LastEditTime: 2023-05-15 13:32:40
+ * @LastEditTime: 2023-05-15 13:38:26
  * @FilePath: \Project-Web\nextjs-blog\pages\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,7 +27,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Person blog in next.js]</p>
+        <p>[Personal blog in next.js]</p>
         <p>
           (This is a sample website - you'll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
