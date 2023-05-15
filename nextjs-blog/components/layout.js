92dd80d11197627088f969c814c751e6c8c7d5e5
Bobby Shu <shu.yun@northeastern.edu>
2023-05-15 09:36:08 +0800

    Modified folder name

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -2,7 +2,7 @@
  * @Author: Bobby Shu shu.yun@northeastern.edu
  * @Date: 2023-05-12 16:00:04
  * @LastEditors: Bobby Shu shu.yun@northeastern.edu
- * @LastEditTime: 2023-05-12 16:39:11
+ * @LastEditTime: 2023-05-15 09:32:56
  * @FilePath: \Project-Web\nextjs-blog\components\layout.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
